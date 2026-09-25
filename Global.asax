fc05b752c64aa6ebb9d5b178c2684832f6ad8a41
Andrei Garcia <andreirg13@gmail.com>
2026-09-24 21:16:50 -0700

    Enforce HTTP to HTTPS redirect

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="CyberApp_FIA.Global" %>
---
+++ Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace CyberApp_FIA
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (!Request.IsSecureConnection)
+            {
+                var secureUrl = new UriBuilder(Request.Url)
+                {
+                    Scheme = Uri.UriSchemeHttps,
+                    Port = Request.Url.IsLoopback ? 44347 : 443
+                };
+
+                Response.RedirectPermanent(secureUrl.Uri.AbsoluteUri, false);
+                Context.ApplicationInstance.CompleteRequest();
+            }
+        }
+    }
+}
